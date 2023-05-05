cda95c9fbc744f26e3c6151895fcb81ee5a2cfc0
Gabriel Miranda <48923787+render41@users.noreply.github.com>
2023-05-05 09:36:37 -0300

    Corrections Code

--- 01-Exercise/app/src/main/java/com/example/samsungocean_androidexercise01/MainActivity.kt
+++ 01-Exercise/app/src/main/java/com/example/samsungocean_androidexercise01/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.samsungocean_androidexercise01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,15 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViews()
+        findResources()
     }
 
-    private fun findViews() {
+    private fun findResources() {
         textInput = findViewById(R.id.editText)
         resultText = findViewById(R.id.resultEditText)
     }
 
-    fun buttonClick(view: View?) {
+    fun buttonClick(view: View) {
         resultText.text = textInput.text
         println(textInput.text)
     }
